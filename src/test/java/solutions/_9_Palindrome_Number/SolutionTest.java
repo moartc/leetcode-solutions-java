713a67e65cabf18e8ecf548a33bec273ee2fe6fd
mo <allenszwester@gmail.com>
2021-10-21 00:16:44 +0200

    Solutions for problems 9 and 13

---
+++ src/test/java/solutions/_9_Palindrome_Number/SolutionTest.java
@@ -0,0 +1,47 @@
+package solutions._9_Palindrome_Number;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+    private Solution solution;
+
+    @BeforeEach
+    void setUp() {
+        solution = new Solution();
+    }
+
+    @Test
+    void isPalindromeTest1() {
+
+        int input = 121;
+        boolean actual = solution.isPalindrome(input);
+        Assertions.assertTrue(actual);
+    }
+
+    @Test
+    void isPalindromeTest2() {
+
+        int input = -121;
+        boolean actual = solution.isPalindrome(input);
+        Assertions.assertFalse(actual);
+    }
+
+    @Test
+    void isPalindromeTest3() {
+
+        int input = 10;
+        boolean actual = solution.isPalindrome(input);
+        Assertions.assertFalse(actual);
+    }
+
+    @Test
+    void isPalindromeTest4() {
+
+        int input = -101;
+        boolean actual = solution.isPalindrome(input);
+        Assertions.assertFalse(actual);
+    }
+}
