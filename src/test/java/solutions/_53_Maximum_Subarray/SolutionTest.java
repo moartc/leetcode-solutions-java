e7e26bd50f977888559cd2f0d3f1bd458016d62c
mo <allenszwester@gmail.com>
2021-10-23 12:17:09 +0200

    Solutions for problems 35, 53 and 58.

---
+++ src/test/java/solutions/_53_Maximum_Subarray/SolutionTest.java
@@ -0,0 +1,36 @@
+package solutions._53_Maximum_Subarray;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+    private Solution solution;
+
+    @BeforeEach
+    void setUp() {
+        solution = new Solution();
+    }
+
+    @Test
+    void maxSubArrayTest1() {
+
+        int input[] = new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        Assertions.assertEquals(6, solution.maxSubArray(input));
+    }
+
+    @Test
+    void maxSubArrayTest2() {
+
+        int input[] = new int[]{1};
+        Assertions.assertEquals(1, solution.maxSubArray(input));
+    }
+
+    @Test
+    void maxSubArrayTest3() {
+
+        int input[] = new int[]{5, 4, -1, 7, 8};
+        Assertions.assertEquals(23, solution.maxSubArray(input));
+    }
+}
