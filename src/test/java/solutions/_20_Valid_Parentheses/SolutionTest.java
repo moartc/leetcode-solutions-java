4744e1b1c6d220202d878ad7daedd78f055cae82
mo <allenszwester@gmail.com>
2021-10-23 10:02:40 +0200

    Solutions for problems 20 and 21.

---
+++ src/test/java/solutions/_20_Valid_Parentheses/SolutionTest.java
@@ -0,0 +1,51 @@
+package solutions._20_Valid_Parentheses;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+    private Solution solution;
+
+    @BeforeEach
+    void setUp() {
+        solution = new Solution();
+    }
+
+    @Test
+    void isValidTest1() {
+        String input = "()";
+        Assertions.assertTrue(solution.isValid(input));
+    }
+
+    @Test
+    void isValidTest2() {
+        String input = "()[]{}";
+        Assertions.assertTrue(solution.isValid(input));
+    }
+
+    @Test
+    void isValidTest3() {
+        String input = "(]";
+        Assertions.assertFalse(solution.isValid(input));
+    }
+
+    @Test
+    void isValidTest4() {
+        String input = "([)]";
+        Assertions.assertFalse(solution.isValid(input));
+    }
+
+    @Test
+    void isValidTest5() {
+        String input = "{[]}";
+        Assertions.assertTrue(solution.isValid(input));
+    }
+
+    @Test
+    void isValidTest6() {
+        String input = "]";
+        Assertions.assertFalse(solution.isValid(input));
+    }
+}
