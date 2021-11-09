dbc0e07b3e4b21d4a773698dbc61f3e37609167a
mo <allenszwester@gmail.com>
2021-11-09 21:45:00 +0100

    Solution for problem 167.

---
+++ src/main/java/solutions/_167_Two_Sum_II_Input_Array_Is_Sorted/Solution.java
@@ -0,0 +1,21 @@
+package solutions._167_Two_Sum_II_Input_Array_Is_Sorted;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+
+        Map<Integer, Integer> map = new HashMap<>(numbers.length);
+        for (int i = 0; i < numbers.length; i++) {
+            int num = numbers[i];
+            if (map.containsKey(target - num)) {
+                int first = map.get(target - num);
+                return new int[]{first + 1, i + 1};
+            } else {
+                map.put(num, i);
+            }
+        }
+        return new int[]{0, 0};
+    }
+}
