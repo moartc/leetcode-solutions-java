b64459e9da9453bb683cb784458e099dc35a6ce3
mo <allenszwester@gmail.com>
2021-11-13 23:17:19 +0100

    Solution for problem 191

---
+++ src/main/java/solutions/algorithms/_191_Number_of_1_Bits/Solution.java
@@ -0,0 +1,13 @@
+package solutions.algorithms._191_Number_of_1_Bits;
+
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        byte counter = 0;
+        while (n != 0) {
+            n &= n - 1;
+            counter++;
+        }
+        return counter;
+    }
+}
---
+++ src/test/java/solutions/algorithms/_191_Number_of_1_Bits/SolutionTest.java
@@ -0,0 +1,39 @@
+package solutions.algorithms._191_Number_of_1_Bits;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+    private Solution solution;
+
+    @BeforeEach
+    void setUp() {
+        solution = new Solution();
+    }
+
+    @Test
+    void hammingWeightTest1() {
+        int input = 11;
+        int expected = 3;
+        int actual = solution.hammingWeight(input);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void hammingWeightTest2() {
+        int input = 128;
+        int expected = 1;
+        int actual = solution.hammingWeight(input);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void hammingWeightTest3() {
+        int input = -3;
+        int expected = 31;
+        int actual = solution.hammingWeight(input);
+        Assertions.assertEquals(expected, actual);
+    }
+}
