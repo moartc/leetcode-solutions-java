c3cdb938ba4bc093933ca8c5395f8cae38f7c101
mo <allenszwester@gmail.com>
2021-11-27 22:44:41 +0100

    Solution for problem 206

---
+++ src/main/java/solutions/algorithms/_206_Reverse_Linked_List/Solution.java
@@ -0,0 +1,31 @@
+package solutions.algorithms._206_Reverse_Linked_List;
+
+import commons.ListNode;
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        if (head.next == null) {
+            return head;
+        }
+        ListNode prev = head;
+        ListNode current = head.next;
+        ListNode next = current.next;
+        prev.next = null;
+        while (next != null && next.next != null) {
+            current.next = prev;
+            prev = current;
+            current = next;
+            next = next.next;
+        }
+        current.next = prev;
+        if (next != null) {
+            next.next = current;
+            return next;
+        } else {
+            return current;
+        }
+    }
+}
