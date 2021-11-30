b4e4906391f67d3d21ad42fb2b3ea7f39d8978bd
mo <allenszwester@gmail.com>
2021-11-30 20:11:15 +0100

    Solution for problem 226

---
+++ src/main/java/solutions/algorithms/_226_Invert_Binary_Tree/Solution.java
@@ -0,0 +1,25 @@
+package solutions.algorithms._226_Invert_Binary_Tree;
+
+import commons.TreeNode;
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        swapChild(root);
+        return root;
+    }
+
+    private void swapChild(TreeNode root) {
+        if (root.left != null) {
+            swapChild(root.left);
+        }
+        if (root.right != null) {
+            swapChild(root.right);
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+    }
+}
