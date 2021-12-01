b7f5291dc2e84a73704ad577549957c9aa285dc8
mo <allenszwester@gmail.com>
2021-12-01 22:09:22 +0100

    Solution for problem 231

---
+++ src/main/java/solutions/algorithms/_231_Power_of_Two/Solution.java
@@ -0,0 +1,9 @@
+package solutions.algorithms._231_Power_of_Two;
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+
+        if (n <= 0) return false;
+        return (n & n-1) == 0;
+    }
+}
