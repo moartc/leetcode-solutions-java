2a5bf7594a190fee15d574421a28785b071c709d
mo <allenszwester@gmail.com>
2022-04-03 21:29:31 +0200

    Solutions to problem 189 and 977

---
+++ src/main/java/solutions/algorithms/_189_Rotate_Array/Solution.java
@@ -0,0 +1,31 @@
+package solutions.algorithms._189_Rotate_Array;
+
+class Solution {
+    public void rotate(int[] nums, int k) {
+
+        int length = nums.length;
+        k = k % length;
+        //reverse last part to move
+        reverse(nums, length - k, length - 1);
+
+        //reverse first part
+        reverse(nums, 0, length - k - 1);
+
+        // reverse all
+        reverse(nums, 0, length - 1);
+    }
+
+    private void swap(int[] arr, int idx1, int idx2) {
+        int temp = arr[idx1];
+        arr[idx1] = arr[idx2];
+        arr[idx2] = temp;
+    }
+
+    private void reverse(int[] arr, int idx1, int idx2) {
+        while (idx1 < idx2) {
+            swap(arr, idx1, idx2);
+            idx1++;
+            idx2--;
+        }
+    }
+}
---
+++ src/main/java/solutions/algorithms/_977_Squares_of_a_Sorted_Array/Solution.java
@@ -0,0 +1,38 @@
+package solutions.algorithms._977_Squares_of_a_Sorted_Array;
+
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+
+        if (nums.length == 1) {
+            return new int[]{nums[0] * nums[0]};
+        }
+        int leftIndex = 0;
+        while (leftIndex < nums.length && nums[leftIndex] < 0) {
+            leftIndex++;
+        }
+        leftIndex--;
+        int[] toReturn = new int[nums.length];
+        int rightIndex = leftIndex + 1;
+        int arrToReturnIndex = 0;
+        while (leftIndex >= 0 && rightIndex <= nums.length - 1) {
+            int left = nums[leftIndex] * nums[leftIndex];
+            int right = nums[rightIndex] * nums[rightIndex];
+            if (left < right) {
+                toReturn[arrToReturnIndex++] = left;
+                leftIndex--;
+            } else {
+                toReturn[arrToReturnIndex++] = right;
+                rightIndex++;
+            }
+        }
+        while (rightIndex < nums.length) {
+            toReturn[arrToReturnIndex++] = nums[rightIndex] * nums[rightIndex];
+            rightIndex++;
+        }
+        while (leftIndex >= 0) {
+            toReturn[arrToReturnIndex++] = nums[leftIndex] * nums[leftIndex];
+            leftIndex--;
+        }
+        return toReturn;
+    }
+}
