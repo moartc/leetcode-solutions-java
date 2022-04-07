9feb2bbc092ee36da11a5fe2d3612ed20adaa516
mo <allenszwester@gmail.com>
2022-04-08 00:08:11 +0200

    Solutions to problem 557 - to rethink / rewrite

---
+++ src/test/java/solutions/algorithms/_557_Reverse_Words_in_a_String_III/SolutionTest.java
@@ -0,0 +1,27 @@
+package solutions.algorithms._557_Reverse_Words_in_a_String_III;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+    private final Solution solution = new Solution();
+
+    @Test
+    void reverseWordsTest1() {
+
+        String s = "Let's take LeetCode contest";
+        String expected = "s'teL ekat edoCteeL tsetnoc";
+        String actual = solution.reverseWords(s);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void reverseWordsTest2() {
+
+        String s = "God Ding";
+        String expected = "doG gniD";
+        String actual = solution.reverseWords(s);
+        Assertions.assertEquals(expected, actual);
+    }
+}
