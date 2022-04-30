8561a1310039fe6b550e9c864a8dc2823a6c9d2d
mo <allenszwester@gmail.com>
2022-04-30 10:10:14 +0200

    Solution to problem 46

---
+++ src/test/java/solutions/algorithms/_46_Permutations/SolutionTest.java
@@ -0,0 +1,77 @@
+package solutions.algorithms._46_Permutations;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SolutionTest {
+
+    private final Solution solution = new Solution();
+
+    @Test
+    void permuteTest1() {
+        int[] nums = new int[]{1, 2, 3};
+        List<List<Integer>> expected = List.of(
+                List.of(1, 2, 3),
+                List.of(1, 3, 2),
+                List.of(2, 1, 3),
+                List.of(2, 3, 1),
+                List.of(3, 1, 2),
+                List.of(3, 2, 1)
+        );
+        assertThat(solution.permute(nums)).hasSameElementsAs(expected);
+    }
+
+    @Test
+    void permuteTest2() {
+        int[] nums = new int[]{0, 1};
+        List<List<Integer>> expected = List.of(
+                List.of(0, 1),
+                List.of(1, 0)
+        );
+        assertThat(solution.permute(nums)).hasSameElementsAs(expected);
+    }
+
+    @Test
+    void permuteTest3() {
+        int[] nums = new int[]{1};
+        List<List<Integer>> expected = List.of(
+                List.of(1)
+        );
+        assertThat(solution.permute(nums)).hasSameElementsAs(expected);
+    }
+
+    @Test
+    void permuteTest4() {
+        int[] nums = new int[]{1, 2, 3, 4};
+        List<List<Integer>> expected = List.of(
+                List.of(1, 2, 3, 4),
+                List.of(2, 1, 3, 4),
+                List.of(3, 1, 2, 4),
+                List.of(1, 3, 2, 4),
+                List.of(2, 3, 1, 4),
+                List.of(3, 2, 1, 4),
+                List.of(3, 2, 4, 1),
+                List.of(2, 3, 4, 1),
+                List.of(4, 3, 2, 1),
+                List.of(3, 4, 2, 1),
+                List.of(2, 4, 3, 1),
+                List.of(4, 2, 3, 1),
+                List.of(4, 1, 3, 2),
+                List.of(1, 4, 3, 2),
+                List.of(3, 4, 1, 2),
+                List.of(4, 3, 1, 2),
+                List.of(1, 3, 4, 2),
+                List.of(3, 1, 4, 2),
+                List.of(2, 1, 4, 3),
+                List.of(1, 2, 4, 3),
+                List.of(4, 2, 1, 3),
+                List.of(2, 4, 1, 3),
+                List.of(1, 4, 2, 3),
+                List.of(4, 1, 2, 3)
+        );
+        assertThat(solution.permute(nums)).hasSameElementsAs(expected);
+    }
+}
