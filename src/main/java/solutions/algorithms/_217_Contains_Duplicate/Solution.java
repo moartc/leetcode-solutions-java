2e5c81f2811ec5c0a4e847cef64d6112bdfce4f7
mo <allenszwester@gmail.com>
2022-05-01 07:43:55 +0200

    New solution to problem 217

--- src/main/java/solutions/algorithms/_217_Contains_Duplicate/Solution.java
+++ src/main/java/solutions/algorithms/_217_Contains_Duplicate/Solution.java
@@ -1,15 +1,28 @@
 package solutions.algorithms._217_Contains_Duplicate;
 
-import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 class Solution {
+
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] == nums[i + 1]) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (!set.add(num)) {
                 return true;
             }
         }
         return false;
     }
+
+// below is my first approach to the problem
+//    public boolean containsDuplicate(int[] nums) {
+//        Arrays.sort(nums);
+//        for (int i = 0; i < nums.length - 1; i++) {
+//            if (nums[i] == nums[i + 1]) {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
 }
