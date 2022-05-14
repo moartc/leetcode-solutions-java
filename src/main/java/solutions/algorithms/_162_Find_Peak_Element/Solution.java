34601409171d6ec6493d52861e4347b3b45b30de
mo <allenszwester@gmail.com>
2022-05-14 07:34:48 +0200

    Solution to problem 162

---
+++ src/main/java/solutions/algorithms/_162_Find_Peak_Element/Solution.java
@@ -0,0 +1,25 @@
+package solutions.algorithms._162_Find_Peak_Element;
+
+class Solution {
+    public int findPeakElement(int[] nums) {
+        if (nums.length == 1) {
+            return 0;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        while (right - left > 1) {
+            int middle = (left + right) / 2;
+            int nextMiddle = middle + 1;
+            if (nums[middle] < nums[nextMiddle]) {
+                left = middle;
+            } else {
+                right = middle;
+            }
+        }
+        if (nums[left] > nums[right]) {
+            return left;
+        } else {
+            return right;
+        }
+    }
+}
