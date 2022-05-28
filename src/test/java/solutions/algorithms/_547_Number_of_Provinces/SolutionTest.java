07152404db12d1713edb00e470e66d6c81dd7103
mo <allenszwester@gmail.com>
2022-05-28 22:30:40 +0200

    Solution to problem 547

---
+++ src/test/java/solutions/algorithms/_547_Number_of_Provinces/SolutionTest.java
@@ -0,0 +1,27 @@
+package solutions.algorithms._547_Number_of_Provinces;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+    private final Solution solution = new Solution();
+
+    @Test
+    void findCircleNumTest1() {
+        int[][] isConnected = new int[][]{{1, 1, 0}, {1, 1, 0}, {0, 0, 1}};
+        Assertions.assertThat(solution.findCircleNum(isConnected)).isEqualTo(2);
+    }
+
+    @Test
+    void findCircleNumTest2() {
+        int[][] isConnected = new int[][]{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
+        Assertions.assertThat(solution.findCircleNum(isConnected)).isEqualTo(3);
+    }
+
+    @Test
+    void findCircleNumTest3() {
+        int[][] isConnected = new int[][]{{1, 0, 0, 1}, {0, 1, 1, 0}, {0, 1, 1, 1}, {1, 0, 1, 1}};
+        Assertions.assertThat(solution.findCircleNum(isConnected)).isOne();
+    }
+}
