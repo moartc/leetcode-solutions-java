8a0cf5def2c7a1d09ddba7f7e493ef2efdc1955a
mo <allenszwester@gmail.com>
2022-06-21 23:02:02 +0200

    Solution to problem 130

---
+++ src/main/java/solutions/algorithms/_130_Surrounded_Regions/Solution.java
@@ -0,0 +1,46 @@
+package solutions.algorithms._130_Surrounded_Regions;
+
+class Solution {
+    public void solve(char[][] board) {
+
+        int m = board.length;
+        int n = board[0].length;
+        boolean[][] visited = new boolean[m][n];
+        for (int i = 0; i < m; i++) {
+            if (board[i][0] == 'O') {
+                connectNeighbour(i, 0, m, n, board, visited);
+            }
+            if (board[i][n - 1] == 'O') {
+                connectNeighbour(i, n - 1, m, n, board, visited);
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (board[m - 1][i] == 'O') {
+                connectNeighbour(m - 1, i, m, n, board, visited);
+            }
+            if (board[0][i] == 'O') {
+                connectNeighbour(0, i, m, n, board, visited);
+            }
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (board[i][j] == 'O' && !visited[i][j]) {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+    }
+
+    void connectNeighbour(int y, int x, int m, int n, char[][] board, boolean[][] visitedNotToFlip) {
+        visitedNotToFlip[y][x] = true;
+        for (int[] move : new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}) {
+            int newY = y + move[0];
+            int newX = x + move[1];
+            if (newY >= 0 && newY < m && newX >= 0 && newX < n && board[newY][newX] == 'O' && !visitedNotToFlip[newY][newX]) {
+                connectNeighbour(newY, newX, m, n, board, visitedNotToFlip);
+            }
+        }
+    }
+}
---
+++ src/test/java/solutions/algorithms/_130_Surrounded_Regions/SolutionTest.java
@@ -0,0 +1,25 @@
+package solutions.algorithms._130_Surrounded_Regions;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+    private final Solution solution = new Solution();
+
+    @Test
+    void solveTest1() {
+        char[][] board = new char[][]{{'X', 'X', 'X', 'X'}, {'X', 'O', 'O', 'X'}, {'X', 'X', 'O', 'X'}, {'X', 'O', 'X', 'X'}};
+        char[][] expected = {{'X', 'X', 'X', 'X'}, {'X', 'X', 'X', 'X'}, {'X', 'X', 'X', 'X'}, {'X', 'O', 'X', 'X'}};
+        solution.solve(board);
+        Assertions.assertThat(board).isEqualTo(expected);
+    }
+
+    @Test
+    void solveTest2() {
+        char[][] board = new char[][]{{'O'}};
+        char[][] expected = {{'O'}};
+        solution.solve(board);
+        Assertions.assertThat(board).isEqualTo(expected);
+    }
+}
