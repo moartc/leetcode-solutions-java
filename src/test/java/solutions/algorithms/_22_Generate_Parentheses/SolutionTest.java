aa5d59c81fa63c58b03209f6064547cbb894eac6
mo <allenszwester@gmail.com>
2022-07-12 23:02:53 +0200

    Test to problem 22

---
+++ src/test/java/solutions/algorithms/_22_Generate_Parentheses/SolutionTest.java
@@ -0,0 +1,26 @@
+package solutions.algorithms._22_Generate_Parentheses;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+class SolutionTest {
+
+    private Solution solution = new Solution();
+
+    @Test
+    void generateParenthesisTest1() {
+        List<String> expected = List.of("((()))", "(()())", "(())()", "()(())", "()()()");
+        List<String> actual = solution.generateParenthesis(3);
+        assertThat(actual).containsExactlyInAnyOrderElementsOf(expected);
+    }
+
+    @Test
+    void generateParenthesisTest2() {
+        List<String> expected = List.of("()");
+        List<String> actual = solution.generateParenthesis(1);
+        assertThat(actual).containsExactlyInAnyOrderElementsOf(expected);
+    }
+}
