dc32e569350048f5b845d3cb8af88c8c9a1da9a3
szwester <allenszwester@gmail.com>
2022-08-21 13:42:54 +0200

    Tests to problem 91.

---
+++ src/test/java/solutions/algorithms/_91_Decode_Ways/SolutionTest.java
@@ -0,0 +1,46 @@
+package solutions.algorithms._91_Decode_Ways;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SolutionTest {
+
+    private final Solution solution = new Solution();
+
+    @Test
+    void numDecodingsTest1() {
+        String s = "12";
+        assertThat(solution.numDecodings(s)).isEqualTo(2);
+    }
+
+    @Test
+    void numDecodingsTest2() {
+        String s = "226";
+        assertThat(solution.numDecodings(s)).isEqualTo(3);
+    }
+
+    @Test
+    void numDecodingsTest3() {
+        String s = "06";
+        assertThat(solution.numDecodings(s)).isEqualTo(0);
+    }
+
+    @Test
+    void numDecodingsTest4() {
+        String s = "1";
+        assertThat(solution.numDecodings(s)).isEqualTo(1);
+    }
+
+    @Test
+    void numDecodingsTest5() {
+        String s = "60";
+        assertThat(solution.numDecodings(s)).isEqualTo(0);
+    }
+
+    @Test
+    void numDecodingsTest6() {
+        String s = "10";
+        assertThat(solution.numDecodings(s)).isEqualTo(1);
+    }
+}
