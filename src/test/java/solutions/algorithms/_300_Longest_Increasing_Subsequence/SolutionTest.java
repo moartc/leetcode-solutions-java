e85943dca0f3bd8ade673d57205618e26e666faa
mo <allenszwester@gmail.com>
2022-10-02 10:05:02 +0200

    Solution to problem 300. Mem 95% but time 23%.

--- src/test/java/solutions/algorithms/_300_Longest_Increasing_Subsequence/SolutionTest.java
+++ src/test/java/solutions/algorithms/_300_Longest_Increasing_Subsequence/SolutionTest.java
@@ -54,4 +54,36 @@ class SolutionTest {
         int actual = solution.lengthOfLIS(nums);
         Assertions.assertThat(actual).isEqualTo(6);
     }
+
+    @Test
+    void lengthOfLISTest7() {
+
+        int[] nums = new int[]{0, 3, 6, 9, 1, 2, 3, 4, 5};
+        int actual = solution.lengthOfLIS(nums);
+        Assertions.assertThat(actual).isEqualTo(6);
+    }
+
+    @Test
+    void lengthOfLISTest8() {
+
+        int[] nums = new int[]{-813, 82, -728, -82, -432, 887, -551, 324, -315, 306, -164, -499, -873, -613, 932, 177, 61, 52, 1000, -710, 372, -306, -584, -332, -500, 407, 399, -648, 290, -866, 222, 562, 993, -338, -590, 303, -16, -134, 226, -648, 909, 582, 177, 899, -343, 55, 629, 248, 333, 1, -921, 143, 629, 981, -435, 681, 844, 349, 613, 457, 797, 695, 485, 15, 710, -450, -775, 961, -445, -905, 466, 942, 995, -289, -397, 434, -14, 34, -903, 314, 862, -441, 507, -966, 525, 624, -706, 39, 152, 536, 874, -364, 747, -35, 446, -608, -554, -411, 987, -354, -700, -34, 395, -977, 544, -330, 596, 335, -612, 28, 586, 228, -664, -841, -999, -100, -620, 718, 489, 346, 450, 772, 941, 952, -560, 58, 999, -879, 396, -101, 897, -1000, -566, -296, -555, 938, 941, 475, -260, -52, 193, 379, 866, 226, -611, -177, 507, 910, -594, -856, 156, 71, -946, -660, -716, -295, -927, 148, 620, 201, 706, 570, -659, 174, 637, -293, 736, -735, 377, -687, -962, 768, 430, 576, 160, 577, -329, 175, 51, 699, -113, 950, -364, 383, 5, 748, -250, -644, -576, -227, 603, 832, -483, -237, 235, 893, -336, 452, -526, 372, -418, 356, 325, -180, 134, -698};
+        int actual = solution.lengthOfLIS(nums);
+        Assertions.assertThat(actual).isEqualTo(25);
+    }
+
+    @Test
+    void lengthOfLISTest9() {
+
+        int[] nums = new int[]{0, 1, 2};
+        int actual = solution.lengthOfLIS(nums);
+        Assertions.assertThat(actual).isEqualTo(3);
+    }
+
+    @Test
+    void lengthOfLISTest10() {
+
+        int[] nums = new int[]{10, 9, 2, 5, 3, 7, 200, 18, 101, 102, 103};
+        int actual = solution.lengthOfLIS(nums);
+        Assertions.assertThat(actual).isEqualTo(7);
+    }
 }
