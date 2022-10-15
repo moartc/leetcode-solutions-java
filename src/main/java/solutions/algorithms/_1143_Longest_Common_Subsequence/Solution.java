93b4e8f63382e1927e08e301a6caffbd958f8010
mo <allenszwester@gmail.com>
2022-10-15 08:36:33 +0200

    Small refactoring for solution to problem 1143 - set first (0) row and column first

--- src/main/java/solutions/algorithms/_1143_Longest_Common_Subsequence/Solution.java
+++ src/main/java/solutions/algorithms/_1143_Longest_Common_Subsequence/Solution.java
@@ -7,21 +7,22 @@ class Solution {
         char[] t1 = text1.toCharArray();
         char[] t2 = text2.toCharArray();
         int[][] arr = new int[t1.length][t2.length];
-        for (int i = 0; i < t1.length; i++) {
-            for (int j = 0; j < t2.length; j++) {
+
+        arr[0][0] = t1[0] == t2[0] ? 1 : 0;
+        for (int i = 1; i < t2.length; i++) {
+            arr[0][i] = arr[0][i - 1] > 0 ? arr[0][i - 1] : t1[0] == t2[i] ? 1 : 0;
+        }
+        for (int i = 1; i < t1.length; i++) {
+            arr[i][0] = arr[i - 1][0] > 0 ? arr[i - 1][0] : t1[i] == t2[0] ? 1 : 0;
+        }
+
+        for (int i = 1; i < t1.length; i++) {
+            for (int j = 1; j < t2.length; j++) {
                 boolean eq = t1[i] == t2[j];
-                if (i != 0 && j != 0) {
-                    if (eq && arr[i][j - 1] == arr[i - 1][j - 1]) {
-                        arr[i][j] = arr[i][j - 1] + 1;
-                    } else {
-                        arr[i][j] = Integer.max(arr[i][j - 1], arr[i - 1][j]);
-                    }
-                } else if (i == 0 && j == 0) {
-                    arr[0][0] = eq ? 1 : 0;
-                } else if (i == 0) {
-                    arr[i][j] = arr[0][j - 1] > 0 ? arr[0][j - 1] : eq ? 1 : 0;
+                if (eq && arr[i][j - 1] == arr[i - 1][j - 1]) {
+                    arr[i][j] = arr[i][j - 1] + 1;
                 } else {
-                    arr[i][0] = arr[i - 1][0] > 0 ? arr[i - 1][0] : eq ? 1 : 0;
+                    arr[i][j] = Integer.max(arr[i][j - 1], arr[i - 1][j]);
                 }
             }
         }
