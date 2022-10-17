43445bf0b94066c9d754bc94512e4808b8767dd9
mo <allenszwester@gmail.com>
2022-10-17 22:09:15 +0200

    Solution to problem 149

---
+++ src/main/java/solutions/algorithms/_149_Max_Points_on_a_Line/Solution.java
@@ -0,0 +1,55 @@
+package solutions.algorithms._149_Max_Points_on_a_Line;
+
+class Solution {
+
+    public int maxPoints(int[][] points) {
+        if (points.length == 1) {
+            return 1;
+        } else if (points.length == 2) {
+            return 2;
+        }
+
+        int max = 0;
+        for (int i = 0; i < points.length - 1; i++) {
+            for (int j = i + 1; j < points.length; j++) {
+                int numberOfPointsOnTheSameLine = getNumOfPointsOnTheSameLine(i, j, points) + 2;
+                if (numberOfPointsOnTheSameLine > max) {
+                    max = numberOfPointsOnTheSameLine;
+                }
+            }
+        }
+        return max;
+    }
+
+    int getNumOfPointsOnTheSameLine(int p1Index, int p2Index, int[][] allPoints) {
+        int counter = 0;
+        for (int i = p2Index; i < allPoints.length; i++) {
+            if (i == p1Index || i == p2Index) {
+                continue;
+            }
+            int[] p1 = allPoints[p1Index];
+            int[] p2 = allPoints[p2Index];
+            int[] p3 = allPoints[i];
+            int xL = p1[0] - p2[0];
+            int yL = p1[1] - p2[1];
+
+            if (yL == 0) {
+                if (p1[1] == p3[1]) {
+                    counter++;
+                }
+                continue;
+            } else if (xL == 0) {
+                if (p1[0] == p3[0]) {
+                    counter++;
+                }
+                continue;
+            }
+            int xToCheck = p3[0] - p2[0];
+            int yToCheck = p3[1] - p2[1];
+            if (xToCheck * yL / (double) xL == yToCheck) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
---
+++ src/test/java/solutions/algorithms/_149_Max_Points_on_a_Line/SolutionTest.java
@@ -0,0 +1,68 @@
+package solutions.algorithms._149_Max_Points_on_a_Line;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+    private final Solution solution = new Solution();
+
+    @Test
+    void maxPointsTest1() {
+        int[][] points = new int[][]{{1, 1}, {2, 2}, {3, 3}};
+        int expected = 3;
+        int actual = solution.maxPoints(points);
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void maxPointsTest2() {
+        int[][] points = new int[][]{{1, 1}, {3, 2}, {5, 3}, {4, 1}, {2, 3}, {1, 4}};
+        int expected = 4;
+        int actual = solution.maxPoints(points);
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void maxPointsTest3() {
+        int[][] points = new int[][]{{9, -25}, {-4, 1}, {-1, 5}, {-7, 7}};
+        int expected = 3;
+        int actual = solution.maxPoints(points);
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void maxPointsTest4() {
+        int[][] points = new int[][]{{3, 2}, {4, 1}, {2, 3}, {1, 4}};
+        int expected = 4;
+        int actual = solution.maxPoints(points);
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void maxPointsTest5() {
+        int[][] points = new int[][]{{0, 1}, {0, 0}, {0, 4}, {0, -2}, {0, -1}, {0, 3}, {0, -4}};
+        int expected = 7;
+        int actual = solution.maxPoints(points);
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void maxPointsTest6() {
+        int[][] points = new int[][]{{0, 0}};
+        int expected = 1;
+        int actual = solution.maxPoints(points);
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void maxPointsTest7() {
+        int[][] points = new int[][]{{2, 3}, {3, 3}, {-5, 3}};
+        int expected = 3;
+        int actual = solution.maxPoints(points);
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+}
+
+
+
