a2d4e9eace7f5273e685a253db6523a5dad44ea3
szwester <allenszwester@gmail.com>
2023-03-30 22:05:46 +0200

    Removing changes causing a time limit

--- src/main/java/solutions/algorithms/_45_Jump_Game_II/Solution.java
+++ src/main/java/solutions/algorithms/_45_Jump_Game_II/Solution.java
@@ -7,21 +7,23 @@ class Solution {
             return 0;
         }
         int i = 0;
-        int jumps = 1;
-        while (i + nums[i] < nums.length - 1) {
-            i += getBest(nums, i);
+        int jumps = 0;
+        while (true) {
+            if (i + nums[i] >= nums.length - 1) {
+                return jumps + 1;
+            }
+            i += getBest(nums, i, nums[i]);
             jumps++;
         }
-        return jumps;
     }
 
-    int getBest(int[] nums, int start) {
+    int getBest(int[] nums, int start, int jump) {
         int max = Integer.MIN_VALUE;
         int dec = 0;
         int toReturn = -1;
-        for (int i = nums[start]; i > 0; i--) {
+        for (int i = jump; i > 0; i--) {
             if (i < nums.length) {
-                int val = nums[start + i] - (dec++);
+                int val = nums[start + i] - dec;
                 dec++;
                 if (val >= max) {
                     max = val;
