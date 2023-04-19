474bd25100b9d0bcb84ec70475c32fc2f5926962
szwester <allenszwester@gmail.com>
2023-04-19 22:28:51 +0200

    Solutions to problem 334 - to improve

---
+++ src/main/java/solutions/algorithms/_334_Increasing_Triplet_Subsequence/Solution.java
@@ -0,0 +1,25 @@
+package solutions.algorithms._334_Increasing_Triplet_Subsequence;
+
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+
+        int lowestI = Integer.MAX_VALUE;
+        int lowestJ = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (nums[i] < lowestI) {
+                lowestI = nums[i];
+                for (int j = i + 1; j < nums.length - 1; j++) {
+                    if (nums[j] < lowestJ && nums[i] < nums[j]) {
+                        lowestJ = nums[j];
+                        for (int k = j + 1; k < nums.length; k++) {
+                            if (nums[j] < nums[k]) {
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
