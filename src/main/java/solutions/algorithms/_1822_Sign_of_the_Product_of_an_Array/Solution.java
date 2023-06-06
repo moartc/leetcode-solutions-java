c05b19643e91c56ad02db3e253cc14b1593a2270
szwester <allenszwester@gmail.com>
2023-06-06 21:57:33 +0200

    Solutions to problem 1822

--- src/main/java/solutions/algorithms/_1822_Sign_of_the_Product_of_an_Array/Solution.java
+++ src/main/java/solutions/algorithms/_1822_Sign_of_the_Product_of_an_Array/Solution.java
@@ -5,9 +5,9 @@ class Solution {
 
         int result = 1;
         for (int num : nums) {
-            if(num == 0) {
+            if (num == 0) {
                 return 0;
-            } else if(num < 0) {
+            } else if (num < 0) {
                 result *= -1;
             }
         }
