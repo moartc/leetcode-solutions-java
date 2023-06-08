33281cae753014f9d5dbe461f9f4dc8f4931c614
szwester <allenszwester@gmail.com>
2023-06-08 22:02:53 +0200

    Solutions to problem 896

---
+++ src/main/java/solutions/algorithms/_896_Monotonic_Array/Solution.java
@@ -0,0 +1,25 @@
+package solutions.algorithms._896_Monotonic_Array;
+
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+
+        if (nums.length == 1) {
+            return true;
+        }
+        boolean isSet = false;
+        boolean isIncrease = false;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] != nums[i]) {
+                if (isSet) {
+                    if ((isIncrease && nums[i] < nums[i - 1]) || (!isIncrease && nums[i] > nums[i - 1])) {
+                        return false;
+                    }
+                } else {
+                    isSet = true;
+                    isIncrease = nums[i] > nums[i - 1];
+                }
+            }
+        }
+        return true;
+    }
+}
