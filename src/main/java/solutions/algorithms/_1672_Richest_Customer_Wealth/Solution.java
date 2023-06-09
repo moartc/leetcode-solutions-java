6492da60c432aea7d2aeb20202a49d1251e7d5ca
szwester <allenszwester@gmail.com>
2023-06-09 22:29:13 +0200

    Solutions to problem 1672

---
+++ src/main/java/solutions/algorithms/_1672_Richest_Customer_Wealth/Solution.java
@@ -0,0 +1,18 @@
+package solutions.algorithms._1672_Richest_Customer_Wealth;
+
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+
+        int max = -1;
+        for (int[] account : accounts) {
+            int sum = 0;
+            for (int i : account) {
+                sum += i;
+            }
+            if (sum > max) {
+                max = sum;
+            }
+        }
+        return max;
+    }
+}
---
+++ src/test/java/solutions/algorithms/_1672_Richest_Customer_Wealth/SolutionTest.java
@@ -0,0 +1,30 @@
+package solutions.algorithms._1672_Richest_Customer_Wealth;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+    private final Solution solution = new Solution();
+
+    @Test
+    void maximumWealthTest1() {
+        int[][] accounts = new int[][]{{1, 2, 3}, {3, 2, 1}};
+        int actual = solution.maximumWealth(accounts);
+        Assertions.assertThat(actual).isEqualTo(6);
+    }
+
+    @Test
+    void maximumWealthTest2() {
+        int[][] accounts = new int[][]{{1, 5}, {7, 3}, {3, 5}};
+        int actual = solution.maximumWealth(accounts);
+        Assertions.assertThat(actual).isEqualTo(10);
+    }
+
+    @Test
+    void maximumWealthTest3() {
+        int[][] accounts = new int[][]{{2, 8, 7}, {7, 1, 3}, {1, 9, 5}};
+        int actual = solution.maximumWealth(accounts);
+        Assertions.assertThat(actual).isEqualTo(17);
+    }
+}
