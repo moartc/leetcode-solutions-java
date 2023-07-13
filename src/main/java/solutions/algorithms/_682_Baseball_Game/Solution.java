f85f80f4aefe4b4454580985ea3dfa15aa23725e
szwester <allenszwester@gmail.com>
2023-07-13 23:21:10 +0200

    Solutions to problem 682

---
+++ src/main/java/solutions/algorithms/_682_Baseball_Game/Solution.java
@@ -0,0 +1,30 @@
+package solutions.algorithms._682_Baseball_Game;
+
+import java.util.LinkedList;
+
+class Solution {
+    public int calPoints(String[] operations) {
+
+        LinkedList<Integer> register = new LinkedList<>();
+
+        for (String operation : operations) {
+            if ("+".equals(operation)) {
+                int penultimate = register.get(register.size() - 2);
+                int last = register.getLast();
+                register.add(penultimate + last);
+            } else if ("D".equals(operation)) {
+                register.add(register.getLast() * 2);
+            } else if ("C".equals(operation)) {
+                register.removeLast();
+            } else { // it's a digit
+                int value = Integer.parseInt(operation);
+                register.add(value);
+            }
+        }
+        int result = 0;
+        for (Integer integer : register) {
+            result += integer;
+        }
+        return result;
+    }
+}
