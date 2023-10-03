5dc2f3f94a56eb8bdda42052d796375211baf552
szwester <allenszwester@gmail.com>
2023-10-03 22:28:14 +0200

    Solution to problem 392.

---
+++ src/main/java/solutions/algorithms/_392_Is_Subsequence/Solution.java
@@ -0,0 +1,18 @@
+package solutions.algorithms._392_Is_Subsequence;
+
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+
+        int i = 0;
+        int j = 0;
+        char[] arr1  = s.toCharArray();
+        char[] arr2  = t.toCharArray();
+        while (i < arr1.length && j < arr2.length) {
+            if (arr1[i] == arr2[j]) {
+                i++;
+            }
+            j++;
+        }
+        return i == s.length();
+    }
+}
