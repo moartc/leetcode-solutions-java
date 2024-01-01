81d67595ef427acf67b7191f05017646445601fa
szwester <allenszwester@gmail.com>
2024-01-01 22:41:16 +0100

    Solutions to problem 73 - constant space solution.

--- src/main/java/solutions/algorithms/_73_Set_Matrix_Zeroes/Solution.java
+++ src/main/java/solutions/algorithms/_73_Set_Matrix_Zeroes/Solution.java
@@ -2,26 +2,51 @@ package solutions.algorithms._73_Set_Matrix_Zeroes;
 
 class Solution {
 
-    // O(n+m) solution
     public void setZeroes(int[][] matrix) {
 
-        boolean[] rows = new boolean[matrix.length];
-        boolean[] cols = new boolean[matrix[0].length];
+        boolean setZeroRow = false;
+        boolean setZeroCol = false;
+        for (int x = 0; x < matrix[0].length; x++) {
+            if (matrix[0][x] == 0) {
+                setZeroRow = true;
+                break;
+            }
+        }
+        for (int[] ints : matrix) {
+            if (ints[0] == 0) {
+                setZeroCol = true;
+                break;
+            }
+        }
 
-        for (int y = 0; y < matrix.length; y++) {
-            for (int x = 0; x < matrix[y].length; x++) {
-                if(matrix[y][x] == 0) {
-                    rows[y] = true;
-                    cols[x] = true;
+        for (int y = 1; y < matrix.length; y++) {
+            for (int x = 1; x < matrix[y].length; x++) {
+                if (matrix[y][x] == 0) {
+                    matrix[0][x] = 0;
+                    matrix[y][0] = 0;
                 }
             }
         }
 
-        for (int y = 0; y < matrix.length; y++) {
-            for (int x = 0; x < matrix[y].length; x++) {
-                if(cols[x] || rows[y]) {
+        for (int y = 1; y < matrix.length; y++) {
+            for (int x = 1; x < matrix[y].length; x++) {
+                if (matrix[0][x] == 0 || matrix[y][0] == 0) {
                     matrix[y][x] = 0;
                 }
+            }
+        }
+
+        // changes for row 0
+        if (setZeroRow) {
+            for (int x = 0; x < matrix[0].length; x++) {
+                matrix[0][x] = 0;
+            }
+        }
+
+        // changes for col 0
+        if (setZeroCol) {
+            for (int y = 0; y < matrix.length; y++) {
+                matrix[y][0] = 0;
             }
         }
     }
