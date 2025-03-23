cb73d108ed8a6e436cb34d60c6f0c426e20812c7
mo <allenszwester@gmail.com>
2025-03-23 22:14:46 +0100

    Fix 238 - solved with O(1) extra space complexity

--- src/main/java/solutions/algorithms/_0_999/_238_Product_of_Array_Except_Self/Solution.java
+++ src/main/java/solutions/algorithms/_0_999/_238_Product_of_Array_Except_Self/Solution.java
@@ -3,24 +3,28 @@ package solutions.algorithms._0_999._238_Product_of_Array_Except_Self;
 class Solution {
     public int[] productExceptSelf(int[] nums) {
 
-        int prod1 = 1;
-        int prod2 = 1;
-        int[] prodRight = new int[nums.length];
-        int[] prodLeft = new int[nums.length];
-        int[] result = new int[nums.length];
+        int[] answer = new int[nums.length];
+        int totalPost = 1;
 
         for (int i = 0; i < nums.length; i++) {
-            prod1 *= nums[i];
-            prodRight[i] = prod1;
-            prod2 *= nums[nums.length-1-i];
-            prodLeft[nums.length-1-i] = prod2;
+            var postVal = nums[nums.length - 1 - i];
+            totalPost *= postVal;
+            answer[nums.length - 1 - i] = totalPost;
         }
-        result[0] = prodLeft[1];
-        result[nums.length-1] = prodRight[nums.length-2];
-        for (int i = 1; i < nums.length-1; i++) {
-            int res = prodRight[i-1] * prodLeft[i+1];
-            result[i] = res;
+
+        int leftProd = 1;
+        for (int i = 0; i < nums.length; i++) {
+            if (i == 0) {
+                leftProd *= nums[i];
+                answer[i] = answer[i + 1];
+            } else if (i == nums.length - 1) {
+                answer[i] = leftProd;
+            } else {
+                var toMultiply = nums[i];
+                answer[i] = leftProd * answer[i + 1];
+                leftProd *= toMultiply;
+            }
         }
-        return result;
+        return answer;
     }
 }
