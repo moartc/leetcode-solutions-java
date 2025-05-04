a3a2d226e974f94d52defad2d0eafc9ac983b53e
mo <allenszwester@gmail.com>
2025-05-04 22:00:27 +0200

    Solution 155 - fixed

--- src/main/java/solutions/algorithms/_0_999/_155_Min_Stack/MinStack.java
+++ src/main/java/solutions/algorithms/_0_999/_155_Min_Stack/MinStack.java
@@ -1,46 +1,87 @@
 package solutions.algorithms._0_999._155_Min_Stack;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 class MinStack {
 
-    private List<Integer> list;
-    private int min;
+    private Node currentNode;
 
     public MinStack() {
-        list = new ArrayList<>();
-        min = Integer.MAX_VALUE;
+
     }
 
     public void push(int val) {
-        if (val < min) {
-            min = val;
+        if (currentNode == null) {
+            currentNode = new Node(val, val, null, null);
+        } else {
+            currentNode.setNext(new Node(val, Math.min(currentNode.getMin(), val), currentNode, null));
+            currentNode = currentNode.next;
         }
-        list.add(val);
     }
 
     public void pop() {
-        int removed = list.remove(list.size() - 1);
-        if (removed == min) {
-            setNewMin();
-        }
+        currentNode = currentNode.prev;
     }
 
     public int top() {
-        return list.get(list.size() - 1);
+        int toReturn = currentNode.val;
+        return toReturn;
     }
 
     public int getMin() {
-        return min;
+        return currentNode.getMin();
     }
 
-    private void setNewMin() {
-        if (list.size() > 0) {
-            min = Collections.min(list);
-        } else {
-            min = Integer.MAX_VALUE;
+    private class Node {
+        int val;
+        int min;
+        Node prev;
+        Node next;
+
+        public Node(int val, int min, Node prev, Node next) {
+            this.val = val;
+            this.min = min;
+            this.prev = prev;
+            this.next = next;
+        }
+
+        public int getVal() {
+            return val;
+        }
+
+        public void setVal(int val) {
+            this.val = val;
+        }
+
+        public Node getPrev() {
+            return prev;
+        }
+
+        public void setPrev(Node prev) {
+            this.prev = prev;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+
+        public int getMin() {
+            return min;
+        }
+
+        public void setMin(int min) {
+            this.min = min;
         }
     }
 }
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
