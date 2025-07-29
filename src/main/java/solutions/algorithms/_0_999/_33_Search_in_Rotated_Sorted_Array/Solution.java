603c0811ad9a9832310ec917ea757fbe4f4c6d5c
mo <allenszwester@gmail.com>
2025-07-29 22:01:05 +0200

    Newer solution to problem 33

--- src/main/java/solutions/algorithms/_0_999/_33_Search_in_Rotated_Sorted_Array/Solution.java
+++ src/main/java/solutions/algorithms/_0_999/_33_Search_in_Rotated_Sorted_Array/Solution.java
@@ -2,38 +2,42 @@ package solutions.algorithms._0_999._33_Search_in_Rotated_Sorted_Array;
 
 class Solution {
     public int search(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
-        int middle;
-        if(!(target >= nums[0] || target <= nums[right])) {
-            return -1;
-        }
-        while (right - left > 1) {
-            middle = (left + right) / 2;
-            int middleValue = nums[middle];
-            if(middleValue == target) {
-                return middle;
-            }
-            if (isInLeftPart(target, nums[left], middleValue)) {
-                right = middle;
-            } else {
-                left = middle+1;
-            }
-        }
-        if(nums[left] == target) {
-            return left;
-        } else if(nums[right] == target) {
-            return right;
-        } else {
-            return -1;
-        }
+
+        return find(0, nums.length - 1, target, nums);
     }
 
-    boolean isInLeftPart(int target, int left, int right) {
-        if(left < right) {
-            return target >= left && target <= right;
+    int find(int leftIdx, int rightIdx, int target, int[] nums) {
+
+        if (leftIdx > rightIdx) {
+            return -1;
+        }
+
+        int middleIdx = (rightIdx + leftIdx) / 2;
+
+        int middleValue = nums[middleIdx];
+
+        if (middleValue == target) {
+            return middleIdx;
+        }
+
+        int leftValue = nums[leftIdx];
+        int rightValue = nums[rightIdx];
+        boolean leftLowerThanMiddle = leftValue <= middleValue;
+
+        if (leftLowerThanMiddle) {
+            if (target >= leftValue && target < middleValue) {
+                return find(leftIdx, middleIdx - 1, target, nums);
+            } else {
+                return find(middleIdx + 1, rightIdx, target, nums);
+            }
         } else {
-            return target <= right || target >= left;
+            if (target > middleValue && target <= rightValue) {
+                return find(middleIdx + 1, rightIdx, target, nums);
+            } else {
+                return find(leftIdx, middleIdx - 1, target, nums);
+            }
         }
     }
 }
+
+// 7 0 1 2 3 4 5
