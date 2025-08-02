e2e5b424377d4389d3a72dd7bbaba7b619b05515
mo <allenszwester@gmail.com>
2025-08-02 18:46:04 +0200

    Improved solution to problem 121

--- src/main/java/solutions/algorithms/_0_999/_121_Best_Time_to_Buy_and_Sell_Stock/Solution.java
+++ src/main/java/solutions/algorithms/_0_999/_121_Best_Time_to_Buy_and_Sell_Stock/Solution.java
@@ -2,23 +2,25 @@ package solutions.algorithms._0_999._121_Best_Time_to_Buy_and_Sell_Stock;
 
 class Solution {
     public int maxProfit(int[] prices) {
-        if (prices.length < 2) {
+
+        if (prices.length <= 1) {
             return 0;
         }
-        int buyPrice = prices[0];
-        int sellPrice = prices[0];
-        int maxProfit = 0;
-        for (int i = 1; i < prices.length; i++) {
-            int currentPrice = prices[i];
-            if (currentPrice > sellPrice) {
-                sellPrice = currentPrice;
-            }
-            if (currentPrice < buyPrice) {
-                maxProfit = Math.max(maxProfit, sellPrice - buyPrice);
-                buyPrice = currentPrice;
-                sellPrice = currentPrice;
+        int bestFound = 0;
+        int i1 = 0;
+        int i2 = 1;
+        while (i2 <= prices.length - 1) {
+
+            int f = prices[i1];
+            int s = prices[i2];
+            if (f < s) {
+                bestFound = Math.max(s - f, bestFound);
+                i2++;
+            } else {
+                i1 = i2;
+                i2++;
             }
         }
-        return Math.max(maxProfit, sellPrice - buyPrice);
+        return bestFound;
     }
 }
