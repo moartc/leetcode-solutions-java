aa53c5e44aa4e75e38fa3d85392f4d36abd6cce3
mo <allenszwester@gmail.com>
2025-08-11 21:27:47 +0200

    Solution to problem 2 - revisited

--- src/main/java/solutions/algorithms/_0_999/_2_Add_Two_Numbers/Solution.java
+++ src/main/java/solutions/algorithms/_0_999/_2_Add_Two_Numbers/Solution.java
@@ -14,39 +14,34 @@ import commons.ListNode;
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode toReturn = l1;
-        int val = l1.val + l2.val;
-        l1.val = (val) % 10;
-        int move = (val) / 10;
-        while (l1.next != null || l2.next != null) {
-            if(l1.next != null && l2.next != null) {
-                val = l1.next.val + l2.next.val + move;
-                l1.next.val = val % 10;
-                move = val / 10;
-                l1 = l1.next;
-                l2 = l2.next;
+
+        ListNode toReturn = null;
+        ListNode newList = null;
+        boolean initialized = false;
+        int quotient = 0;
+        while (l1 != null || l2 != null) {
+
+            int fv = l1 != null ? l1.val : 0;
+            int sv = l2 != null ? l2.val : 0;
+            int sum = fv + sv;
+            int toAdd = (quotient + sum) % 10;
+            quotient = (quotient + sum) / 10;
+            ListNode newNode = new ListNode(toAdd);
+            if (!initialized) {
+                toReturn = newNode;
+                newList = newNode;
+                initialized = true;
             } else {
-                if(l1.next == null) {
-                    l1.next = l2.next;
-                }
-                val = l1.next.val + move;
-                l1.next.val = val % 10;
-                move = val / 10;
-                l1 = l1.next;
-                while(l1.next != null) {
-                    int v = l1.next.val + move;
-                    move = v / 10;
-                    l1.next.val = v % 10;
-                    l1 = l1.next;
-                }
-                if(move == 1) {
-                    l1.next = new ListNode(1);
-                }
-                return toReturn;
+                newList.next = newNode;
+                newList = newList.next;
             }
+            l1 = l1 == null ? null : l1.next;
+            l2 = l2 == null ? null : l2.next;
         }
-        if(move == 1) {
-            l1.next = new ListNode(1);
+        
+        if (quotient != 0) {
+            ListNode newNode = new ListNode(quotient);
+            newList.next = newNode;
         }
         return toReturn;
     }
