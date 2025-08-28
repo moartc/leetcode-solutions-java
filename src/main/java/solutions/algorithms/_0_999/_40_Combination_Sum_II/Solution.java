67e98e888e027b87ea01e81bc2cc90a752fc700b
mo <allenszwester@gmail.com>
2025-08-28 23:01:11 +0200

    Solution to problem 40 - revisited

--- src/main/java/solutions/algorithms/_0_999/_40_Combination_Sum_II/Solution.java
+++ src/main/java/solutions/algorithms/_0_999/_40_Combination_Sum_II/Solution.java
@@ -5,29 +5,59 @@ import java.util.Arrays;
 import java.util.List;
 
 class Solution {
+
+
+    /*
+    Actually something similar to the previous problem "39. Combination Sum"
+
+    notes:
+    It might make sense to sort the elements
+    then I can avoid cases where for ... 1, 7, 1... I create [1,7] and [7,1]
+
+    I make from [10, 1, 2, 7, 6, 1, 5] this -> [1, 1, 2, 5, 6, 7, 10]
+
+    I should start with the same number only once
+
+     */
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+
+        List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(candidates);
-        List<List<Integer>> toReturn = new ArrayList<>();
-        generate(candidates, new ArrayList<>(), 0, target, 0, toReturn);
-        return toReturn;
+        // start with the same number only once
+        int i = 0;
+        ArrayList<Integer> emptyArray = new ArrayList<>();
+        while (i < candidates.length) {
+            int valueForI = candidates[i];
+            createSet(i, 0, emptyArray, result, candidates, target);
+            int newI = i;
+            while (newI < candidates.length && candidates[newI] == valueForI) {
+                newI++;
+            }
+            i = newI;
+        }
+        return result;
     }
 
-    void generate(int[] candidates, List<Integer> current, int currentSum, int target, int lastIndex, List<List<Integer>> toReturn) {
-        if (currentSum == target) {
-            toReturn.add(current);
-        } else {
-            int counter = -1;
-            for (int i = lastIndex; i < candidates.length; i++) {
-                counter++;
-                if (currentSum + candidates[i] > target) {
-                    return;
-                } else if (counter > 0 && candidates[i - 1] == candidates[i]) {
-                    continue;
-                } else if (currentSum + candidates[i] <= target) {
-                    List<Integer> newCurrent = new ArrayList<>(current);
-                    newCurrent.add(candidates[i]);
-                    generate(candidates, newCurrent, currentSum + candidates[i], target, i + 1, toReturn);
+
+    void createSet(int currentIdx, int currentSum, List<Integer> currentSet, List<List<Integer>> resultList, int[] candidates, int target) {
+        int value = candidates[currentIdx];
+
+        if (value + currentSum == target) {
+            List<Integer> newSet = new ArrayList<>(currentSet);
+            newSet.add(value);
+            resultList.add(newSet);
+        } else if (value + currentSum < target) {
+            int i = currentIdx + 1;
+            List<Integer> newSet = new ArrayList<>(currentSet);
+            newSet.add(value);
+            while (i < candidates.length) {
+                createSet(i, currentSum + value, newSet, resultList, candidates, target);
+                int valueForI = candidates[i];
+                int newI = i;
+                while (newI < candidates.length && candidates[newI] == valueForI) {
+                    newI++;
                 }
+                i = newI;
             }
         }
     }
