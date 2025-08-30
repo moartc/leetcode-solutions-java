640477c73bbaaac53376fc05c9b655fe93ce30cb
mo <allenszwester@gmail.com>
2025-08-30 15:53:59 +0200

    Solution to problem 435 - revisited

--- src/main/java/solutions/algorithms/_0_999/_435_Non_overlapping_Intervals/Solution.java
+++ src/main/java/solutions/algorithms/_0_999/_435_Non_overlapping_Intervals/Solution.java
@@ -4,23 +4,48 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 class Solution {
+
+    /*
+    Similar to the problem 57
+
+    First, sort by start and end points.
+    If two intervals start at the same position, remove the longer one.
+
+    This is an additional test case: [[0,16],[1,3],[3,4],[4,6]]
+    I cannot just take the first one and remove all subsequent overlapping intervals.
+    It seems I should remove the one that ends later
+
+     */
     public int eraseOverlapIntervals(int[][] intervals) {
 
-        Arrays.sort(intervals, Comparator.comparingInt(o -> o[0]));
+        if (intervals.length == 1) {
+            return 0;
+        }
 
+        // with this sorting, my solution beats 80%
+        // with Arrays.sort(intervals, Comparator.comparingInt(o -> o[0])); - only beats 20%.
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0];
+            }
+        });
+
+        int currentEnd = intervals[0][1];
         int counter = 0;
-        int[] prev = intervals[0];
         for (int i = 1; i < intervals.length; i++) {
-            int[] current = intervals[i];
-            if (current[0] < prev[1]) {
-                if (prev[1] > current[1]) {
-                    prev = current;
+            int[] interval = intervals[i];
+            if (currentEnd > interval[0]) {
+                // remove one - the one which ends later
+                if (currentEnd > interval[1]) {
+                    currentEnd = interval[1];
                 }
                 counter++;
             } else {
-                prev = current;
+                currentEnd = interval[1];
             }
         }
         return counter;
     }
+
 }
--- src/test/java/solutions/algorithms/_0_999/_435_Non_overlapping_Intervals/SolutionTest.java
+++ src/test/java/solutions/algorithms/_0_999/_435_Non_overlapping_Intervals/SolutionTest.java
@@ -2,7 +2,6 @@ package solutions.algorithms._0_999._435_Non_overlapping_Intervals;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import solutions.algorithms._0_999._435_Non_overlapping_Intervals.Solution;
 
 class SolutionTest {
 
@@ -34,5 +33,12 @@ class SolutionTest {
         int[][] intervals = new int[][]{{0, 2}, {1, 3}, {2, 4}, {3, 5}, {4, 6}};
         int result = solution.eraseOverlapIntervals(intervals);
         Assertions.assertThat(result).isEqualTo(2);
+    }
+
+    @Test
+    void eraseOverlapIntervalsTest5() {
+        int[][] intervals = new int[][]{{0, 16}, {1, 3}, {3, 5}, {5, 6}};
+        int result = solution.eraseOverlapIntervals(intervals);
+        Assertions.assertThat(result).isEqualTo(1);
     }
 }
