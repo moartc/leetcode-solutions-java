8400cfa95aebd9436c5ec8e4ed874fa2db788450
mo <allenszwester@gmail.com>
2025-09-04 19:48:09 +0200

    Solution to problem 198 - revisited

--- src/main/java/solutions/algorithms/_0_999/_198_House_Robber/Solution.java
+++ src/main/java/solutions/algorithms/_0_999/_198_House_Robber/Solution.java
@@ -1,31 +1,48 @@
 package solutions.algorithms._0_999._198_House_Robber;
 
+import java.util.Arrays;
+
 class Solution {
 
-    int max = -1;
-    int[] maxes;
+    /*
+    It looks like I should either start at 0 or 1 and then skip 1 (go to the (i+2)th) or 2 (go to the (i+3)th)
+     */
+
+    // cache with the higher amount of money I can rob starting from the ith house
+    int cache[];
 
     public int rob(int[] nums) {
 
-        maxes = new int[nums.length];
         if (nums.length == 1) {
             return nums[0];
         }
-        robNext(nums, 0, nums[0]);
-        robNext(nums, 1, nums[1]);
-        return max;
+        cache = new int[nums.length + 1];
+        Arrays.fill(cache, -1);
+        solve(0, nums);
+        solve(1, nums);
+
+        return Math.max(cache[0], cache[1]);
     }
 
-    void robNext(int[] all, int currentIndex, int currentSum) {
-        maxes[currentIndex] = currentSum;
-        if (currentSum > max) {
-            max = currentSum;
+    private int solve(int i, int[] nums) {
+
+        if (i == nums.length - 2 || i == nums.length - 1) {
+            if (cache[i] == -1) {
+                cache[i] = nums[i];
+            }
+        } else if (i == nums.length - 3) { // from here I can only jump to the last one
+            if (cache[i] == -1) {
+                int current = nums[i];
+                cache[i] = current + solve(i + 2, nums);
+                ;
+            }
+        } else {
+            if (cache[i] == -1) {
+                int current = nums[i];
+                int max = current + Math.max(solve(i + 2, nums), solve(i + 3, nums));
+                cache[i] = max;
+            }
         }
-        if (currentIndex + 2 <= all.length - 1 && maxes[currentIndex + 2] < currentSum + all[currentIndex + 2]) {
-            robNext(all, currentIndex + 2, currentSum + all[currentIndex + 2]);
-        }
-        if (currentIndex + 3 <= all.length - 1 && maxes[currentIndex + 3] < currentSum + all[currentIndex + 3]) {
-            robNext(all, currentIndex + 3, currentSum + all[currentIndex + 3]);
-        }
+        return cache[i];
     }
 }
