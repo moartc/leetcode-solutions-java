333a29cb150888e6a84c6052e3a73614db62d534
mo <allenszwester@gmail.com>
2025-09-09 19:45:05 +0200

    Solution to problem 300 - revisited

--- src/main/java/solutions/algorithms/_0_999/_300_Longest_Increasing_Subsequence/Solution.java
+++ src/main/java/solutions/algorithms/_0_999/_300_Longest_Increasing_Subsequence/Solution.java
@@ -1,51 +1,78 @@
 package solutions.algorithms._0_999._300_Longest_Increasing_Subsequence;
 
+import java.util.*;
+
 class Solution {
 
-    int[] indexToPath;
-    int max = 1;
+    // recursion with caching, beats 30%
+    int[] cache;
+
     public int lengthOfLIS(int[] nums) {
 
-        indexToPath = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            if (max > nums.length - i) {
-                break;
-            }
-            int found;
-            if (indexToPath[i] != 0) {
-                found = indexToPath[i];
-            } else {
-                found = collect(i, nums);
-            }
-            if (found > max) {
-                max = found;
+        cache = new int[nums.length];
+        Arrays.fill(cache, -1);
+
+        int best = -1;
+        for(int i = 0; i < nums.length; i++) {
+            best = Math.max(solve(i, nums), best);
+        }
+
+        return best;
+    }
+
+    int solve(int idx, int[] nums) {
+
+        if(idx == nums.length-1) {
+            return 1;
+        }
+
+        if(cache[idx] != -1) {
+            return cache[idx];
+        }
+
+        // from the current index I can go to any which is further in the nums array and has a greater value
+        int current = nums[idx];
+        int bestSolved = 1;
+        for (int i = idx+1; i < nums.length; i++) {
+            int v = nums[i];
+            if(v > current) {
+                bestSolved = Math.max(1 + solve(i, nums), bestSolved);
             }
         }
-        return max;
+        cache[idx] = bestSolved;
+        return bestSolved;
     }
 
 
-    int collect(int start, int[] nums) {
-        int maxFound = 0;
-        if (indexToPath[start] != 0) {
-            return indexToPath[start];
-        } else {
-            for (int i = start + 1; i < nums.length; i++) {
-                if (nums[start] < nums[i]) {
-                    int collected;
-                    if(indexToPath[i] != 0) {
-                        collected = indexToPath[i];
-                    } else {
-                        collected = collect(i, nums);
-                    }
-                    if (maxFound < collected) {
-                        maxFound = collected;
-                    }
+    // below the first solution which beats ~20%
+    /*
+    I can track in a sorted map the max increasing subsequence to the current value
+    update:
+    my solution below, but it's O(n^2)
+     */
+    public int lengthOfLIS_v1(int[] nums) {
+
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+
+        map.put(nums[0], 1);
+
+        int currentMaxToReturn = 1;
+        for (int i = 1; i < nums.length; i++) {
+            int val = nums[i];
+            // find max path for lower keys
+            int maxFound = 0;
+            for (Integer value : map.headMap(val).values()) {
+                if (value > maxFound) {
+                    maxFound = value;
                 }
             }
-            indexToPath[start] = maxFound + 1;
-            return maxFound + 1;
+            if (currentMaxToReturn < maxFound + 1) {
+                currentMaxToReturn = maxFound + 1;
+            }
+            map.put(val, maxFound + 1);
         }
+
+        return currentMaxToReturn;
     }
 }
 
--- src/test/java/solutions/algorithms/_0_999/_300_Longest_Increasing_Subsequence/SolutionTest.java
+++ src/test/java/solutions/algorithms/_0_999/_300_Longest_Increasing_Subsequence/SolutionTest.java
@@ -2,7 +2,6 @@ package solutions.algorithms._0_999._300_Longest_Increasing_Subsequence;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import solutions.algorithms._0_999._300_Longest_Increasing_Subsequence.Solution;
 
 class SolutionTest {
 
