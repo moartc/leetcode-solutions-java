feb6fd1bb45d62ba71a8a488c3e28f5f63022f6e
mo <allenszwester@gmail.com>
2025-09-26 19:43:48 +0200

    forgotten solution to problem 66

--- src/main/java/solutions/algorithms/_0_999/_66_Plus_One/Solution.java
+++ src/main/java/solutions/algorithms/_0_999/_66_Plus_One/Solution.java
@@ -1,24 +1,35 @@
 package solutions.algorithms._0_999._66_Plus_One;
 
 class Solution {
+
+    /*
+    Just carrying over the reminder
+     */
     public int[] plusOne(int[] digits) {
 
-        int index = digits.length - 1;
-        while (true) {
-            int digit = digits[index];
-            if (digit == 9 && index != 0) {
-                digits[index] = 0;
-                index--;
-            } else if (digit == 9) {
-                int[] arrayToReturn = new int[digits.length + 1];
-                System.arraycopy(digits, 1, arrayToReturn, 2, digits.length - 1);
-                arrayToReturn[1] = 0;
-                arrayToReturn[0] = 1;
-                return arrayToReturn;
+        int lastIdx = digits.length - 1;
+        int incrementedLastVal = digits[lastIdx] + 1;
+        int reminder = incrementedLastVal == 10 ? 1 : 0;
+        digits[lastIdx] = incrementedLastVal % 10;
+        for (int i = lastIdx - 1; i >= 0; i--) {
+            int value = digits[i] + reminder;
+            if (value == 10) {
+                digits[i] = 0;
+                reminder = value / 10;
             } else {
-                digits[index]++;
-                return digits;
+                digits[i] = value;
+                reminder = 0;
             }
+        }
+
+        if (reminder > 0) {
+            // there is still a reminder - I need a new array
+            int[] newArr = new int[digits.length + 1];
+            System.arraycopy(digits, 0, newArr, 1, digits.length);
+            newArr[0] = reminder;
+            return newArr;
+        } else {
+            return digits;
         }
     }
 }
