db6398989ecc0bfb11917af37dc8c9cd932cce95
mo <allenszwester@gmail.com>
2025-10-07 22:57:18 +0200

    tests to problem 10

---
+++ src/test/java/solutions/algorithms/_0_999/_10_Regular_Expression_Matching/SolutionTest.java
@@ -0,0 +1,57 @@
+package solutions.algorithms._0_999._10_Regular_Expression_Matching;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+    private final Solution solution = new Solution();
+
+    @Test
+    void isMatch1() {
+        String s = "aa";
+        String p = "a";
+        boolean answer = solution.isMatch(s, p);
+        Assertions.assertThat(answer).isFalse();
+    }
+
+    @Test
+    void isMatch2() {
+        String s = "aa";
+        String p = "a*";
+        boolean answer = solution.isMatch(s, p);
+        Assertions.assertThat(answer).isTrue();
+    }
+
+    @Test
+    void isMatch3() {
+        String s = "ab";
+        String p = ".*";
+        boolean answer = solution.isMatch(s, p);
+        Assertions.assertThat(answer).isTrue();
+    }
+
+    @Test
+    void isMatch4() {
+        String s = "aab";
+        String p = "c*a*b";
+        boolean answer = solution.isMatch(s, p);
+        Assertions.assertThat(answer).isTrue();
+    }
+
+    @Test
+    void isMatch5() {
+        String s = "abcdeeefg";
+        String p = "ab.de*fg";
+        boolean answer = solution.isMatch(s, p);
+        Assertions.assertThat(answer).isTrue();
+    }
+
+    @Test
+    void isMatch6() {
+        String s = "abbbb";
+        String p = "ab*b";
+        boolean answer = solution.isMatch(s, p);
+        Assertions.assertThat(answer).isTrue();
+    }
+}
