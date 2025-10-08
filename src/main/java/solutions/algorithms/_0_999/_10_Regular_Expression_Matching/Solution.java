38fe52730de59d5e1ae92ab0c866cafab565741f
mo <allenszwester@gmail.com>
2025-10-08 20:20:01 +0200

    solution to problem 10 - mini refactoring

--- src/main/java/solutions/algorithms/_0_999/_10_Regular_Expression_Matching/Solution.java
+++ src/main/java/solutions/algorithms/_0_999/_10_Regular_Expression_Matching/Solution.java
@@ -9,20 +9,18 @@ class Solution {
     update:
     I'm trying a recursive solution with some caching, because I don't know how to solve it with DP
     it actually beats 100% xD
-    todo cleanup / fix / find a better solution
      */
 
-    int[][] cache;
+    byte[][] cache;
 
     public boolean isMatch(String s, String p) {
 
         char[] pArray = p.toCharArray();
         char[] sArray = s.toCharArray();
-
         // -1 - unset, 0 - false, 1 - true
-        cache = new int[sArray.length + 1][pArray.length + 1];
-        for (int[] ints : cache) {
-            Arrays.fill(ints, -1);
+        cache = new byte[sArray.length + 1][pArray.length + 1];
+        for (byte[] ints : cache) {
+            Arrays.fill(ints, (byte) -1);
         }
         return rec(0, 0, sArray, pArray);
     }
@@ -32,7 +30,6 @@ class Solution {
         if (cache[i][j] != -1) {
             return cache[i][j] == 1;
         }
-
         if (i == sArr.length && j == pArr.length) {
             return true;
         }
@@ -49,7 +46,6 @@ class Solution {
         }
         char patternChar = pArr[j];
         char stringChar = sArr[i];
-//        System.out.println("match char = " + stringChar + ", with patter = " + patternChar);
         if (patternChar == '.' || stringChar == patternChar) {
             // I can check the next one
             boolean answer;
@@ -64,14 +60,14 @@ class Solution {
                 // match . only once
                 answer = rec(i + 1, j + 1, sArr, pArr);
             }
-            cache[i][j] = answer ? 1 : 0;
+            cache[i][j] = (byte) (answer ? 1 : 0);
             return answer;
         } else {
             // string doesn't match
             if (j < pArr.length - 1 && pArr[j + 1] == '*') {
                 // match it once
                 boolean answer = rec(i, j + 2, sArr, pArr);
-                cache[i][j] = answer ? 1 : 0;
+                cache[i][j] = (byte) (answer ? 1 : 0);
                 return answer;
             } else {
                 cache[i][j] = 0;
