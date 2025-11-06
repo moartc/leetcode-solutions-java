3257cdb56c09f5bfbcc3ca5f20951fb47763f4e6
mo <allenszwester@gmail.com>
2025-11-06 22:03:29 +0100

    solution to problem 219 - revisited

--- src/main/java/solutions/algorithms/_0_999/_219_Contains_Duplicate_II/Solution.java
+++ src/main/java/solutions/algorithms/_0_999/_219_Contains_Duplicate_II/Solution.java
@@ -1,18 +1,34 @@
 package solutions.algorithms._0_999._219_Contains_Duplicate_II;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 class Solution {
+
+    /*
+    ok, so I have 2 indices, i and j and the abs distance between them should be <=k
+    that means I have a window of length 2k.
+    I can start with an empty set, add the element at index 0, then 1, then 2, expanding it up to size 2k.
+    Each time I add an element, I check if it's already in the set - if so, return true.
+    When the window reaches its max size, I remove the leftmost element (which I can access by index)
+    and add the next one on the right, again checking if it's already in the set.
+     */
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            Integer value = map.get(nums[i]);
-            if (value != null && Math.abs(value - i) <= k) {
-                return true;
-            } else {
-                map.put(nums[i], i);
+
+        if (k == 0) {
+            // I cannot find 2 distinct indices where the distance is equal to 0
+            return false;
+        }
+        Set<Integer> set = new HashSet<>();
+        int i = 0;
+        while (i < nums.length) {
+            if (set.size() >= k + 1) {
+                set.remove(nums[i - k - 1]);
             }
+            if (!set.add(nums[i])) {
+                return true;
+            }
+            i++;
         }
         return false;
     }
--- src/test/java/solutions/algorithms/_0_999/_219_Contains_Duplicate_II/SolutionTest.java
+++ src/test/java/solutions/algorithms/_0_999/_219_Contains_Duplicate_II/SolutionTest.java
@@ -3,7 +3,6 @@ package solutions.algorithms._0_999._219_Contains_Duplicate_II;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import solutions.algorithms._0_999._219_Contains_Duplicate_II.Solution;
 
 class SolutionTest {
 
@@ -37,4 +36,30 @@ class SolutionTest {
         boolean actual = solution.containsNearbyDuplicate(nums, k);
         Assertions.assertFalse(actual);
     }
+
+    @Test
+    void containsNearbyDuplicateTest4() {
+        int[] nums = new int[]{1,2,3,1,2,1,3};
+        int k = 1;
+        boolean actual = solution.containsNearbyDuplicate(nums, k);
+        Assertions.assertFalse(actual);
+    }
+
+    @Test
+    void containsNearbyDuplicateTest5() {
+        int[] nums = new int[]{1,4,2,3,1,2};
+        int k = 3;
+        boolean actual = solution.containsNearbyDuplicate(nums, k);
+        Assertions.assertTrue(actual);
+    }
+
+    @Test
+    void containsNearbyDuplicateTest6() {
+        int[] nums = new int[]{4,1,2,3,1,5};
+        int k = 3;
+        boolean actual = solution.containsNearbyDuplicate(nums, k);
+        Assertions.assertTrue(actual);
+    }
+
+
 }
