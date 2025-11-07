0bc7949db31e9cc27870be5134b2f5214b1f205e
mo <allenszwester@gmail.com>
2025-11-07 21:30:08 +0100

    solution to problem 209 - revisited

--- src/main/java/solutions/algorithms/_0_999/_209_Minimum_Size_Subarray_Sum/Solution.java
+++ src/main/java/solutions/algorithms/_0_999/_209_Minimum_Size_Subarray_Sum/Solution.java
@@ -1,29 +1,43 @@
 package solutions.algorithms._0_999._209_Minimum_Size_Subarray_Sum;
 
 class Solution {
+
+    /*
+    Start from the same index. If the total (initially the value at index 0) is lower than the
+    target, move the right pointer forward and add the next value to the current sum.
+    If the current sum is greater than the target, check if the current length is smaller than the previously found one.
+    Then move the left pointer to decrease the sum. If it's below the target, move the right pointer
+    if it's above, move the left one.
+     */
     public int minSubArrayLen(int target, int[] nums) {
-        int first = 0;
-        int second = 0;
-        int length = nums.length;
-        int sum = nums[0];
-        int result = 0;
-        while (first < length) {
-            if (sum >= target) {
-                var res = second - first + 1;
-                if (res < result || result == 0) {
-                    result = res;
+
+        int l = 0;
+        int r = 0;
+        int bestFound = Integer.MAX_VALUE;
+        int currentSum = nums[0];
+        while (l <= r) {
+
+            if (currentSum >= target) {
+                // new best
+                int currLength = r - l + 1;
+                if (currLength < bestFound) {
+                    bestFound = currLength;
                 }
-                sum -= nums[first];
-                first++;
-            } else {
-                second++;
-                if (second < length) {
-                    sum += nums[second];
-                } else {
-                    break;
-                }
+                // decrease the sum
+                currentSum -= nums[l];
+                l++;
+            } else if (r < nums.length - 1) {
+                r++;
+                currentSum += nums[r];
+            } else { // right is already at the last index
+                break;
             }
         }
-        return result;
+        if (bestFound == Integer.MAX_VALUE) {
+            // not found
+            return 0;
+        } else {
+            return bestFound;
+        }
     }
 }
--- src/test/java/solutions/algorithms/_0_999/_209_Minimum_Size_Subarray_Sum/SolutionTest.java
+++ src/test/java/solutions/algorithms/_0_999/_209_Minimum_Size_Subarray_Sum/SolutionTest.java
@@ -2,7 +2,6 @@ package solutions.algorithms._0_999._209_Minimum_Size_Subarray_Sum;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import solutions.algorithms._0_999._209_Minimum_Size_Subarray_Sum.Solution;
 
 class SolutionTest {
 
