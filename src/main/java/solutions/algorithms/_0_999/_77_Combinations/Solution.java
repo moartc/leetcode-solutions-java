93752993e68c631cbf6ebdf09fcc23b2d79d70d4
mo <allenszwester@gmail.com>
2025-12-21 21:08:16 +0100

    solution to problem 77 - revisited

--- src/main/java/solutions/algorithms/_0_999/_77_Combinations/Solution.java
+++ src/main/java/solutions/algorithms/_0_999/_77_Combinations/Solution.java
@@ -1,28 +1,47 @@
 package solutions.algorithms._0_999._77_Combinations;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 class Solution {
+
+    /*
+    Recursive function that starts at the first number and can make 2 decisions
+    - add the current number to the list
+    - skip the current number
+    then we call the same function passing the updated list for the next number
+    at the beginning of this function I can check if the size of the current list is equal to k,
+    if so I add it to the list of results.
+
+    It works but it beats only 9.9%
+    Trying another oslution with backtracking - it's a little better - 33%
+
+     */
+
+    List<List<Integer>> result;
+
     public List<List<Integer>> combine(int n, int k) {
 
-        List<List<Integer>> listToReturn = new ArrayList<>();
-        append(0, n, k, new LinkedList<>(), listToReturn);
-        return listToReturn;
+        if (k > n) {
+            return new ArrayList<>();
+        }
+        result = new ArrayList<>();
+        add(n, k, new ArrayList<>(), 1);
+        return result;
     }
 
-    void append(int start, int n, int k, List<Integer> listToAppend, List<List<Integer>> finalList) {
-        if (listToAppend.size() < k) {
-            for (int i = start + 1; i <= n; i++) {
-                if (k - listToAppend.size() <= (n  - i+1)) {
-                    List<Integer> newList = new LinkedList<>(listToAppend);
-                    newList.add(i);
-                    append(i, n, k, newList, finalList);
-                }
-            }
-        } else if (listToAppend.size() == k) {
-            finalList.add(listToAppend);
+    void add(int n, int k, List<Integer> currentList, int currentInt) {
+        if (currentList.size() == k) {
+            result.add(new ArrayList<>(currentList));
+        } else if (currentInt > n) {
+            return;
+        } else {
+            // go further with the same list
+            currentList.add(currentInt);
+            add(n, k, currentList, currentInt + 1);
+            // when I am back, delete the last one
+            currentList.remove(currentList.size() - 1);
+            add(n, k, currentList, currentInt + 1);
         }
     }
 }
