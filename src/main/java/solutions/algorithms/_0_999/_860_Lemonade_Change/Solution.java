5d356400e4923d2ba938ba987f40497fa272c23f
mo <allenszwester@gmail.com>
2026-01-25 14:32:58 +0100

    solution to problem 860 - revisited

--- src/main/java/solutions/algorithms/_0_999/_860_Lemonade_Change/Solution.java
+++ src/main/java/solutions/algorithms/_0_999/_860_Lemonade_Change/Solution.java
@@ -1,35 +1,38 @@
 package solutions.algorithms._0_999._860_Lemonade_Change;
 
 class Solution {
+
+    /*
+    track number of 20, 10, and 5 bills
+    when I have to give the rest I subtract from the higher possible bill
+     */
     public boolean lemonadeChange(int[] bills) {
 
-        // number of 5$, 10$ ,d 20$
-        int[] arr = new int[]{0, 0, 0};
-
+        int n5 = 0;
+        int n10 = 0;
         for (int bill : bills) {
-            if (bill == 5) { // only increase number of 5$
-                arr[0]++;
+            if (bill == 5) {
+                n5++;
             } else if (bill == 10) {
-                if (arr[0] > 0) { // if can give back 5$ then do it - decrement, and add 10$
-                    arr[0]--;
-                    arr[1]++;
-                } else { // if cannot give back 5$ return false
+                // I have to use 5
+                if (n5 > 0) {
+                    n5--;
+                    n10++;
+                } else {
                     return false;
                 }
-            } else if (bill == 20) {
-                if (arr[0] > 0 && arr[1] > 0) { // can give back 5$ and 10$
-                    arr[0]--;
-                    arr[1]--;
-                    arr[2]++;
-                } else if (arr[0] >= 3) { // can give back 3 x 5$
-                    arr[0] -= 3;
-                    arr[2]++;
+            } else { // it has to be 20
+                // firs try 10 + 5
+                if (n10 > 0 && n5 > 0) {
+                    n10--;
+                    n5--;
+                } else if (n5 >= 3) {
+                    n5 -= 3;
                 } else {
                     return false;
                 }
             }
         }
-        // all iterations were fine
         return true;
     }
 }
