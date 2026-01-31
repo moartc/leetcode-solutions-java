33b6106f070183c0004915b5b6a54ce311270607
mo <allenszwester@gmail.com>
2026-01-31 15:18:33 +0100

    solution to problem 1071 - revisited

--- src/main/java/solutions/algorithms/_1000_1999/_1071_Greatest_Common_Divisor_of_Strings/Solution.java
+++ src/main/java/solutions/algorithms/_1000_1999/_1071_Greatest_Common_Divisor_of_Strings/Solution.java
@@ -1,36 +1,44 @@
 package solutions.algorithms._1000_1999._1071_Greatest_Common_Divisor_of_Strings;
 
 class Solution {
-    public static int gcd(int a, int b) {
-        if (b == 0) {
-            return a;
-        }
-        return gcd(b, a % b);
-    }
 
+    /*
+    kinda brute force, but not the most stupid
+    Select the shorter one and then, going from the max size down to 1, try whether each character matches
+    I can skip cases where a substring of size x cannot divide both strings
+     */
     public String gcdOfStrings(String str1, String str2) {
-        int div = gcd(str1.length(), str2.length());
-        while (div >= 1) {
-            if (str1.length() % div == 0 && str2.length() % div == 0) {
-                if (canDivide(str1, str2, div) && canDivide(str2, str2, div)) {
-                    return str1.substring(0, div);
+
+        String shorter = str1;
+        String longer = str2;
+        if (str1.length() > str2.length()) {
+            shorter = str2;
+            longer = str1;
+        }
+        for (int i = shorter.length(); i > 0; i--) {
+            if (shorter.length() % i == 0 && longer.length() % i == 0) {
+                if (isDivisor(shorter, longer, i)) {
+                    return shorter.substring(0, i);
                 }
             }
-            div--;
         }
         return "";
     }
 
-    private boolean canDivide(String s, String div, int commDiv) {
-        var divIdx = 0;
-        for (int i = 0; i < s.toCharArray().length; i++) {
-            if (s.charAt(i) != div.charAt(divIdx)) {
+    boolean isDivisor(String s, String l, int shortL) {
+        int i = 0;
+        while (i < l.length()) {
+            int idx = i % shortL;
+            char expectedChar = s.charAt(idx);
+            if (i < s.length()) {
+                if (s.charAt(i) != expectedChar) {
+                    return false;
+                }
+            }
+            if (l.charAt(i) != expectedChar) {
                 return false;
             }
-            divIdx++;
-            if (divIdx == commDiv) {
-                divIdx = 0;
-            }
+            i++;
         }
         return true;
     }
