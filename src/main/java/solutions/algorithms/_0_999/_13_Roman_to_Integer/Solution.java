bb434fe75c4297508c872be9555ef43b9be314f7
mo <allenszwester@gmail.com>
2026-01-31 16:30:11 +0100

    solution to problem 13 - revisited

--- src/main/java/solutions/algorithms/_0_999/_13_Roman_to_Integer/Solution.java
+++ src/main/java/solutions/algorithms/_0_999/_13_Roman_to_Integer/Solution.java
@@ -3,29 +3,40 @@ package solutions.algorithms._0_999._13_Roman_to_Integer;
 import java.util.Map;
 
 class Solution {
-    Map<Character, Integer> symbolToValue = Map.of(
-            'I', 1,
-            'V', 5,
-            'X', 10,
-            'L', 50,
-            'C', 100,
-            'D', 500,
-            'M', 1000
-    );
 
+    /*
+    first create a char to int map
+    then go through the string, for special characters / cases like
+    IV, IX, XL, XC, CD and CM I can create special handling
+    then just sum the values and jump to the next char
+     */
     public int romanToInt(String s) {
-        char[] reverseInput = new StringBuffer(s).reverse().toString().toCharArray();
-        int result = 0;
-        int lastReadValue = 0;
 
-        for (char c : reverseInput) {
-            int currentValue = symbolToValue.get(c);
-            if (lastReadValue > currentValue) {
-                result -= currentValue;
-            } else {
-                result += currentValue;
+        Map<Character, Integer> m = Map.of(
+                'I', 1,
+                'V', 5,
+                'X', 10,
+                'L', 50,
+                'C', 100,
+                'D', 500,
+                'M', 1000
+        );
+
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+
+            char c = s.charAt(i);
+            int v1 = m.get(c);
+            if (i < s.length() - 1) {
+                // if there is something later
+                char c2 = s.charAt(i + 1);
+                int v2 = m.get(c2);
+                if(v1 < v2) {
+                    result -= v1;
+                    continue;
+                }
             }
-            lastReadValue = currentValue;
+            result += v1;
         }
         return result;
     }
--- src/test/java/solutions/algorithms/_0_999/_13_Roman_to_Integer/SolutionTest.java
+++ src/test/java/solutions/algorithms/_0_999/_13_Roman_to_Integer/SolutionTest.java
@@ -3,7 +3,6 @@ package solutions.algorithms._0_999._13_Roman_to_Integer;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import solutions.algorithms._0_999._13_Roman_to_Integer.Solution;
 
 class SolutionTest {
 
