d23eb8ff7c5c5f518f9397ffabcf25dafb8ac5a8
mo <allenszwester@gmail.com>
2026-02-28 07:57:29 +0100

    solution to problem 417 - revisited

--- src/main/java/solutions/algorithms/_0_999/_417_Pacific_Atlantic_Water_Flow/Solution.java
+++ src/main/java/solutions/algorithms/_0_999/_417_Pacific_Atlantic_Water_Flow/Solution.java
@@ -5,74 +5,60 @@ import java.util.List;
 
 class Solution {
 
-    /*
-    I could mark (in another array or 2 if needed) all cells that border either of 2 oceans.
-    Then, starting from the top-left, I check if I can reach the  Pacific Ocean.
-    The same applies to the Atlantic Ocean,
-    but in this case I process the cells starting from the bottom-right and moving  upwards-leftwards.
+    int[][] indices = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
-    Then, from every cell with access to an ocean, I can visit all cells that have the same or higher height
-    and mark them as also having access.
-
-    update:
-    during the initial marking I also visit neighbours
-
-    beats: 97.72%
-     */
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
 
-        boolean[][] canReachPacific = new boolean[heights.length][heights[0].length];
-        boolean[][] canReachAtlantic = new boolean[heights.length][heights[0].length];
+        int m = heights.length;
+        int n = heights[0].length;
 
-        // pacific part vertically
-        for (int y = 0; y < heights.length; y++) {
-            visitAndMark(y, 0, canReachPacific, heights, true);
+        boolean[][] canAccessPacific = new boolean[m][n];
+        boolean[][] canAccessAtlantic = new boolean[m][n];
+
+        for (int i = 0; i < m; i++) {
+            canAccessPacific[i][0] = true;
+            canAccessAtlantic[i][n - 1] = true;
         }
-        // pacific part horizontally
-        for (int x = 0; x < heights[0].length; x++) {
-            visitAndMark(0, x, canReachPacific, heights, true);
-        }
-        // atlantic part vertically
-        for (int y = 0; y < heights.length; y++) {
-            visitAndMark(y, heights[0].length - 1, canReachAtlantic, heights, true);
-        }
-        // atlantic part horizontally
-        for (int x = 0; x < heights[0].length; x++) {
-            visitAndMark(heights.length - 1, x, canReachAtlantic, heights, true);
+        for (int i = 0; i < n; i++) {
+            canAccessPacific[0][i] = true;
+            canAccessAtlantic[m - 1][i] = true;
         }
 
-        List<List<Integer>> answer = new ArrayList<>();
-        // iterate through both new arrays and select cells where both values are true
-        for (int y = 0; y < heights.length; y++) {
-            for (int x = 0; x < heights[0].length; x++) {
-                if (canReachPacific[y][x] && canReachAtlantic[y][x]) {
-                    answer.add(List.of(y, x));
+        for (int i = 0; i < m; i++) {
+            markVisit(new int[]{i, 0}, heights, canAccessPacific);
+            markVisit(new int[]{i, n - 1}, heights, canAccessAtlantic);
+        }
+        for (int i = 0; i < n; i++) {
+            markVisit(new int[]{0, i}, heights, canAccessPacific);
+            markVisit(new int[]{m - 1, i}, heights, canAccessAtlantic);
+        }
+
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (canAccessPacific[i][j] && canAccessAtlantic[i][j]) {
+                    result.add(List.of(i, j));
                 }
             }
         }
-        return answer;
+        return result;
     }
 
-    void visitAndMark(int y, int x, boolean[][] accessMap, int[][] heights, boolean firstVisit) {
+    void markVisit(int[] start, int[][] heights, boolean[][] canAccessOcean) {
 
-        if (accessMap[y][x] && !firstVisit) {
-            return;
-        }
-        accessMap[y][x] = true;
-
-        int currentHeight = heights[y][x];
-
-        if (y - 1 >= 0 && heights[y - 1][x] >= currentHeight) {
-            visitAndMark(y - 1, x, accessMap, heights, false);
-        }
-        if (y + 1 < heights.length && heights[y + 1][x] >= currentHeight) {
-            visitAndMark(y + 1, x, accessMap, heights, false);
-        }
-        if (x - 1 >= 0 && heights[y][x - 1] >= currentHeight) {
-            visitAndMark(y, x - 1, accessMap, heights, false);
-        }
-        if (x + 1 < heights[0].length && heights[y][x + 1] >= currentHeight) {
-            visitAndMark(y, x + 1, accessMap, heights, false);
+        int yS = start[0];
+        int xS = start[1];
+        for (int[] index : indices) {
+            int diffY = index[0];
+            int diffX = index[1];
+            int newY = yS + diffY;
+            int newX = xS + diffX;
+            if (newY >= 0 && newY < heights.length && newX >= 0 && newX < heights[0].length) {
+                if (heights[newY][newX] >= heights[yS][xS] && !canAccessOcean[newY][newX]) {
+                    canAccessOcean[newY][newX] = true;
+                    markVisit(new int[]{newY, newX}, heights, canAccessOcean);
+                }
+            }
         }
     }
 }
